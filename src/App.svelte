ee6cba8ae060de46fa3b6dbc800a8b3f66d3b737
Tarik <tarikmerrylees@gmail.com>
2022-09-07 11:24:53 +0100

    add global stylesheet

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,14 @@
 <script>
 	import Header from './lib/components/modules/Header.svelte';
+	import Button from './lib/components/atoms/Button.svelte';
 </script>
 
 <main>
   <Header/>
+  <Button label="A Button"/>
 </main>
+
+
+<style>
+  @import './lib/styles/global.scss'
+</style>
---
+++ src/lib/components/atoms/Button.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let label
+</script>
+
+<button class="Button">{label}</button>
+
+<style>
+.Button {
+  background: blue;
+  border: none;
+  border-radius: 3px;
+  color: white;
+  
+}
+</style>
